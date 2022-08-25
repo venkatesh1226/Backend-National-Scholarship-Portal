5d9007e7e240dc2a0d1ad04f5740f44d6293c7bc
venkatesh1226 <venkatesh1226m@gmail.com>
2022-08-25 16:19:46 +0530

    starter code

---
+++ src/main/java/com/lti/nsp/NspApplication.java
@@ -0,0 +1,13 @@
+package com.lti.nsp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NspApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NspApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lti/nsp/NspApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lti.nsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NspApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
